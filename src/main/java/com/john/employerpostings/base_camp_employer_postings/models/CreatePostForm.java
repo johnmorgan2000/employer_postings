c2e51fb44fd7cf894943ec64178486bd64a006d5
John <jmorgan@basecampcodingacademy.org>
2019-03-07 12:50:15 -0600

    Added getters and setters.

--- src/main/java/com/john/employerpostings/base_camp_employer_postings/models/CreatePostForm.java
+++ src/main/java/com/john/employerpostings/base_camp_employer_postings/models/CreatePostForm.java
@@ -16,4 +16,45 @@ public class CreatePostForm{
 
     public String applyUrl;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(String benefits) {
+        this.benefits = benefits;
+    }
+
+    public String getApplyUrl() {
+        return applyUrl;
+    }
+
+    public void setApplyUrl(String applyUrl) {
+        this.applyUrl = applyUrl;
+    }
+
+    
 }
