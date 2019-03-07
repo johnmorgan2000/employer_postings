70923d34ae1364f8a81349277db430c9cc214b91
John <jmorgan@basecampcodingacademy.org>
2019-03-07 12:49:13 -0600

    Added constructor and used repository to get infomation displayed on the page.

--- src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/LandingController.java
+++ src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/LandingController.java
@@ -1,13 +1,25 @@
 package com.john.employerpostings.base_camp_employer_postings.controllers;
 
+import com.john.employerpostings.base_camp_employer_postings.repositories.PostgresJobPostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LandingController {
+    PostgresJobPostRepository jpRepository;
+
+    @Autowired
+    public LandingController(PostgresJobPostRepository repository){
+        jpRepository = repository;
+    }
     
     @GetMapping("/")
-    public String getLanding(){
+    public String getLanding(Model model){
+        Integer count = jpRepository.countRows();
+        model.addAttribute("count", count);
         return "landing";
     }
     
