1ba1237a1faa44af619488a7b11f43818e1faefc
John <jmorgan@basecampcodingacademy.org>
2019-03-07 13:18:20 -0600

    Created findAllJobs to retrieve all the jobs in the table.

--- src/main/java/com/john/employerpostings/base_camp_employer_postings/repositories/PostgresJobPostRepository.java
+++ src/main/java/com/john/employerpostings/base_camp_employer_postings/repositories/PostgresJobPostRepository.java
@@ -1,5 +1,9 @@
 package com.john.employerpostings.base_camp_employer_postings.repositories;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import com.john.employerpostings.base_camp_employer_postings.models.JobPost;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +36,19 @@ public class PostgresJobPostRepository{
             jp.getApplyUrl(),
             jp.getPostedDate()
         );
+    }
+
+    public List<JobPost> findAllJobs(){
+        return jdbc.query("SELECT * FROM employer_posts;", this::mapToJobPost);
+    }
+
+    public JobPost mapToJobPost(ResultSet rs, int rowNum) throws SQLException {
+        return new JobPost(
+        rs.getString("employer_name"), 
+        rs.getString("address"), 
+        rs.getString("position"), 
+        rs.getString("benefits"), 
+        rs.getString("apply_url"), 
+        rs.getDate("posted_date"));
     }
 }
