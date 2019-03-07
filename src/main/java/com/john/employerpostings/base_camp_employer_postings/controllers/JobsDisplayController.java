1466db3d316e8dcef51ceb3a35045dd7fb9bae71
John <jmorgan@basecampcodingacademy.org>
2019-03-07 13:28:59 -0600

    Created this class to added jobs to the jobs_display page.

---
+++ src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/JobsDisplayController.java
@@ -0,0 +1,28 @@
+package com.john.employerpostings.base_camp_employer_postings.controllers;
+
+import java.util.List;
+
+import com.john.employerpostings.base_camp_employer_postings.models.JobPost;
+import com.john.employerpostings.base_camp_employer_postings.repositories.PostgresJobPostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JobsDisplayController {
+    PostgresJobPostRepository jpRepository;
+
+    @Autowired
+    public JobsDisplayController(PostgresJobPostRepository repository){
+        jpRepository = repository;
+    }
+
+    @GetMapping("/jobs")
+    public String index(Model model){
+        List<JobPost> jobs = jpRepository.findAllJobs();
+        model.addAttribute("jobs", jobs);
+        return "jobs_display";
+    }
+}
