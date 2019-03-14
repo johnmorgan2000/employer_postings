122f0574a2b86284d72682805c2f53bcbf108d7d
John <jmorgan@basecampcodingacademy.org>
2019-03-14 11:50:34 -0500

    Added comments to the user side of details page.

--- src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/JobDetailsController.java
+++ src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/JobDetailsController.java
@@ -1,5 +1,8 @@
 package com.john.employerpostings.base_camp_employer_postings.controllers;
 
+import java.util.List;
+
+import com.john.employerpostings.base_camp_employer_postings.models.Comment;
 import com.john.employerpostings.base_camp_employer_postings.repositories.PostgresJobPostRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +24,12 @@ public class JobDetailsController{
     public String index(Model model, @PathVariable(value = "id") String id){
         var detail = jpRepository.findJobById(Integer.parseInt(id));
         if (detail.isPresent()){
+            List<Comment> coms =  jpRepository.findPostComments( Integer.parseInt(id));
             model.addAttribute("detail", detail.get());
-        return "job_detail";
+            model.addAttribute("comments", coms);
+            return "job_detail";
         }else{
             return "404";
         }
-        
     }
 }
