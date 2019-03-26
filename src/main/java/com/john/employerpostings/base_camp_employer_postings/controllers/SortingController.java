36a78589b2148613c45b80c89cbb996734320d67
John <jmorgan@basecampcodingacademy.org>
2019-03-26 14:12:36 -0500

    Made sortForJobs and sortForAdmin to filter thorugh both sides jobs display.

--- src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/SortingController.java
+++ src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/SortingController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SortingController{
@@ -21,9 +23,24 @@ public class SortingController{
     }
 
     @PostMapping("/jobs")
-    public String index(Model model, SortingForm form){
-        List<JobPost> jobs;
+    public String sortForJobs(Model model, SortingForm form){
         String result = form.getSortBy();
+        List<JobPost> jobs = returnSortedJobs(result);
+        model.addAttribute("jobs", jobs);
+        return "jobs_display";
+    }
+
+    @PostMapping("/admin")
+    public String sortForAdmin(Model model, SortingForm form){
+        String result = form.getSortBy();
+        List<JobPost> jobs = returnSortedJobs(result);
+        model.addAttribute("jobs", jobs);
+        return "admin_jobs";
+    }
+
+
+    public List<JobPost> returnSortedJobs(String result){
+        List<JobPost> jobs;
         if(result.equals("newToOld")){
             jobs = jpRepository.findAllByNewest();
         }else if (result.equals("oldToNew")){
@@ -32,7 +49,9 @@ public class SortingController{
         else{
             jobs = jpRepository.findAllByNewest();
         }
-        model.addAttribute("jobs", jobs);
-        return "jobs_display";
+        return jobs;
     }
+
+    
+    
 }
