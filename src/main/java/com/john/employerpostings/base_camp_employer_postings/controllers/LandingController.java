f8b6dbf780b842cefccfaf4dd606142928a0fe4c
John <jmorgan@basecampcodingacademy.org>
2019-03-28 13:01:42 -0500

    Called getTopFive().
    This will get the top five most viewed jop posts and add them to the
    page.

--- src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/LandingController.java
+++ src/main/java/com/john/employerpostings/base_camp_employer_postings/controllers/LandingController.java
@@ -1,5 +1,8 @@
 package com.john.employerpostings.base_camp_employer_postings.controllers;
 
+import java.util.List;
+
+import com.john.employerpostings.base_camp_employer_postings.models.JobPost;
 import com.john.employerpostings.base_camp_employer_postings.repositories.PostgresJobPostRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +22,11 @@ public class LandingController {
     @GetMapping("/")
     public String getLanding(Model model){
         Integer count = jpRepository.countRows();
+        List<JobPost> jobs = jpRepository.getTopFive();
         model.addAttribute("count", count);
+        model.addAttribute("jobs", jobs);
         return "landing";
     }
     
+
 }
